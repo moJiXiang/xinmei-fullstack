3733236f1b3708702f75bff5cdb8de05f8d4e582
moJiXiang <mojixiang1102@gmail.com>
2015-05-26 12:00:54 +0800

    fix bug

--- config/config.js
+++ config/config.js
@@ -12,8 +12,8 @@ var config = {
     apiBaseUri: '/v1/api',
     port: 3000,
     // db: 'mongodb://localhost/xinmei-development',
+    db: 'mongodb://xinmei:xinmei@localhost/xinmei-development',
     db2: 'mongodb://dbs:SeeFaItH4120#@119.254.108.220:7530/stock'
-    db: 'mongodb://xinmei:xinmei@localhost/xinmei-development'
     // db: 'mongodb://192.168.14.126:27017/Xinmei'
   },
 
