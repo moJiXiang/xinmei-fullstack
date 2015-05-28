7031f8fc286812fee1d73edf33fc1356dd72ec29
moJiXiang <mojixiang1102@gmail.com>
2015-05-28 10:50:24 +0800

    change process.env.NODE_ENV

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'development';
+    env = process.env.NODE_ENV || 'production';
 var config = {
   development: {
     root: rootPath,
